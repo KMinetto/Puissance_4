61d89f65b1879345e2d6ccf938306465362e93f3
Kilian MINETTO <killian.minetto@gmail.com>
2022-02-27 22:59:56 +0100

    Displaying table in terminal

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 let puissance4;
-
 const nbColumn = 7, nbLine = 6;
+const playerOneChar = "X";
+const playerTwoChar = "O";
 
 puissance4 = initEmptyTable(nbColumn, nbLine, 0);
-console.log(puissance4);
+displayPuissance4(puissance4, playerOneChar, playerTwoChar);
 
 /**
  * Create an empty table for the game
- * @param {number} nbColumn
- * @param {number} nbLine
- * @param {*} char
+ * @param {Number} nbColumn Column of table
+ * @param {Number} nbLine Row of table
+ * @param {*} char Char to use for empty cell in table
  * @return {*[][]}
  */
 function initEmptyTable(nbColumn, nbLine, char = '') {
@@ -22,4 +23,28 @@ function initEmptyTable(nbColumn, nbLine, char = '') {
         table.push(line);
     }
     return table;
+}
+
+/**
+ * Display personalized table
+ * @param {Array<String>} tab Table to use
+ * @param {String} p1Char Char of the Player 1
+ * @param {String} p2Char Char of the Player 2
+ */
+function displayPuissance4(tab, p1Char, p2Char) {
+    for (let i = 0; i < tab.length; i++) {
+        let line = "";
+        for (let j = 0; j < tab[i].length; j++) {
+            line += "| "
+            if (tab[i][j] === 0) {
+                line += "_";
+            } else if (tab[i][j] === 1) {
+                line += p1Char;
+            } else if (tab[i][j] === 2) {
+                line += p2Char;
+            }
+            line += " |";
+        }
+        console.log(line);
+    }
 }
